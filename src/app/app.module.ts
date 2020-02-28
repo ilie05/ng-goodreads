4b76f43e72d64ce55241522b8c49adb720884cbe
ilie_petrasco <petras.moil@gmail.com>
2020-02-28 15:22:10 +0200

    Fetch data from json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,29 @@ import { FilterSearchPipe } from './shared/filter-search.pipe';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { BookTableComponent } from './book-table/book-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     FilterSearchPipe,
-    SearchBoxComponent
+    SearchBoxComponent,
+    BookTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/book.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Book } from '../shared/book-model';
+import { retry, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor(private http: HttpClient) { }
+
+  private handleError(error: HttpErrorResponse){
+    if(error.error instanceof ErrorEvent){
+      console.log(`An error occured: ${error.error.message}`);
+    }else{
+      console.error(`Backend returned: ${error.status}, ` +
+      `body was: ${error.error}`);
+    }
+
+    return throwError('oops, something wnet wrong :(');
+  }
+
+  fetchBooks(){
+    return this.http.get<Book[]>('/books', httpOptions)
+      .pipe(
+        retry(3),
+        catchError(this.handleError)
+      )
+  }
+
+
+}
